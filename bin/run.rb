bca90d8eafd03ea7ae36436d02609439d63eb3dd
rrosztoczy <rrosztoczy@gmail.com>
2019-05-08 13:11:57 -0400

    integrated first level of crud into normal menu functionality

--- bin/run.rb
+++ bin/run.rb
@@ -1,15 +1,18 @@
 require_relative '../config/environment'
 require '/Users/ryanrosztoczy/Development/Projects/Gems/cli_builder/lib/cli_builder/menu.rb'
 
-CliBuilder::Crud.get_tables
-CliBuilder::Crud.build_model_menu
 
 # #Generate initial application title
 welcome_title
 
 #Buid main menu and application functionality
-main_menu
+def crud_menu
+  CliBuilder::Crud.build_model_menu
+end
 
+  main_menu_array = [:start_new_ride, :ride_menu, :location_menu, :crud_menu]
+  main_menu = CliBuilder::Menu.new(title: "Main Menu", menu_options: main_menu_array)
+  main_menu.build_menu
 
 
 
--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -1,7 +1,4 @@
 
 #Build main menu and application functionality
 ##Main menu options inlclude: go to ride menu, go to who rides menu, go to view locations menu
-def main_menu
-  main_menu_array = [:start_new_ride, :ride_menu, :location_menu]
-  application_builder("Main Menu", main_menu_array)
-end
+
